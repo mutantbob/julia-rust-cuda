0a67bd0c3c49eb6cc776029ac9534e47803a95a2
Robert Forsman <git@thoth.purplefrog.com>
2026-05-11 08:23:36 -0400

    minor refactoring/clean-up

--- src/main.rs
+++ src/main.rs
@@ -65,40 +65,24 @@ pub fn julia(
     }
 }
 
-pub fn count_julia(z: Complex32, c: Complex32, COUNT: u32) -> u32 {
+pub fn count_julia(z: Complex32, c: Complex32, max_iter: u32) -> u32 {
     let Complex32 { re: x, im: y } = z;
-    if false {
-        return (y * 255.0) as _;
-    } else if true {
-        let mut z = Complex32::new(x, y);
-        for i in 0..COUNT {
-            if escaped(&z) {
-                // println!("escaped {z:?} after {i} cycles");
-                // return (z.re.abs() * 64.0) as _;
-                return i;
-            }
-            let x1 = z.re * z.re - z.im * z.im + c.re;
-            let y1 = z.re * z.im + z.im * z.re + c.im;
-            z = Complex::new(x1, y1);
+    let mut z = Complex32::new(x, y);
+    for i in 0..max_iter {
+        if escaped(&z) {
+            // println!("escaped {z:?} after {i} cycles");
+            // return (z.re.abs() * 64.0) as _;
+            return i;
         }
-    } else {
-        let c = x;
-        let mut z = c;
-        for i in 0..COUNT {
-            if false {
-                return i;
-            }
-            z = z * z + c;
-        }
+        let x1 = z.re * z.re - z.im * z.im + c.re;
+        let y1 = z.re * z.im + z.im * z.re + c.im;
+        z = Complex::new(x1, y1);
     }
 
-    COUNT
+    max_iter
 }
 
 fn main() {
-    let ctx = CudaContext::new(0).expect("Failed to create CUDA context");
-    let stream = ctx.default_stream();
-
     let grid = {
         let r = 512;
         let ncols = r;
@@ -114,9 +98,31 @@ fn main() {
             y0: -height / 2.0,
         }
     };
-    let count: usize = grid.cell_count();
+    let cx = -0.5125;
+    let cy = 0.5213;
+    let c_host = if true {
+        on_gpu(&grid, cx, cy)
+    } else {
+        on_cpu(&grid, cx, cy)
+    };
+
+    if false {
+        println!("{c_host:?}");
+    }
+    {
+        let ofname = "/tmp/x.pgm";
+        write_to_pgm(&c_host, grid.ncols, grid.nrows, ofname).unwrap();
+        println!("wrote to {ofname}");
+    }
+}
+
+fn on_gpu(grid: &JuliaGrid, cx: f32, cy: f32) -> Vec<u32> {
+    let ctx = CudaContext::new(0).expect("Failed to create CUDA context");
+    let stream = ctx.default_stream();
 
     println!("doot 1");
+
+    let count: usize = grid.cell_count();
 
     let mut c_dev = DeviceBuffer::<u32>::zeroed(&stream, count).unwrap();
 
@@ -127,39 +133,24 @@ fn main() {
 
     println!("doot 2");
 
-    let cx = -0.5125;
-    let cy = 0.5213;
-    let c_host = if true {
-          cuda_launch! {
-                kernel: julia,
-                stream: stream,
-                module: module,
-                config: LaunchConfig::for_num_elems(count as u32),
-                args: [ slice_mut(c_dev),
-                    grid.ncols,
-                    grid.x0,
-                    grid.y0, grid.dx, grid.dy,
-                    cx,
-        cy,
-                ]
-            }
-        .expect("Kernel launch failed");
+    cuda_launch! {
+            kernel: julia,
+            stream: stream,
+            module: module,
+            config: LaunchConfig::for_num_elems(count as u32),
+            args: [ slice_mut(c_dev),
+                grid.ncols,
+                grid.x0,
+                grid.y0, grid.dx, grid.dy,
+                cx,
+    cy,
+            ]
+        }
+    .expect("Kernel launch failed");
 
-        println!("doot 3");
+    println!("doot 3");
 
-        c_dev.to_host_vec(&stream).unwrap()
-    } else {
-        on_cpu(&grid, cx, cy)
-    };
-
-    if true {
-        println!("{c_host:?}");
-    }
-    {
-        let ofname = "/tmp/x.pgm";
-        write_to_pgm(&c_host, grid.ncols, grid.nrows, ofname).unwrap();
-        println!("wrote to {ofname}");
-    }
+    c_dev.to_host_vec(&stream).unwrap()
 }
 
 fn on_cpu(grid: &JuliaGrid, cx: f32, cy: f32) -> Vec<u32> {
